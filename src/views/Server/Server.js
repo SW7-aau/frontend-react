4bd422b6febae2d831733cd4d2ffb2ce3f45b6fa
Mads <madhan16@student.aau.dk>
2020-11-08 20:11:14 +0100

    Mads some minor alterations

--- src/views/Server/Server.js
+++ src/views/Server/Server.js
@@ -45,8 +45,8 @@ const columns = [
     sort: true,
   },
   {
-    dataField: "DestinationIP",
-    text: "Destination IP",
+    dataField: "Destination",
+    text: "Destination",
     sort: true,
   },
   {
@@ -55,13 +55,8 @@ const columns = [
     sort: true,
   },
   {
-    dataField: "SourceIP",
-    text: "Source IP",
-    sort: true,
-  },
-  {
-    dataField: "SourceName",
-    text: "Source Name",
+    dataField: "Source",
+    text: "Source",
     sort: true,
   },
   {
@@ -69,6 +64,11 @@ const columns = [
     text: "Source Port",
     sort: true,
   },
+  {
+    dataField: "Layer",
+    text: "Layer",
+    sort: true,
+  }
 ];
 
 const products = [
@@ -108,10 +108,16 @@ export default function ServerDetails() {
   const classes = useStyles();
   const [data, setData] = useState([]);
   const [serverStats, setServerStats] = useState([]);
-  const { SearchBar } = Search;  
+  const { SearchBar } = Search;
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("http://localhost:5000/server_status");
+      const result = await axios("http://217.69.10.141:5000/get-resources", {
+        params: {
+          node_id: "testidqwe",
+          from: "2020-09-04 11:50:23",
+          to: "2020-12-04 11:50:23"
+        }
+      });
       setServerStats(result.data);
     };
     fetchData();
@@ -131,29 +137,32 @@ export default function ServerDetails() {
     labels: serverStats.map((status) => status.time_stamp),
     series: [serverStats.map((status) => status.bandwidth)],
   };
-  
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get("http://localhost:5000/Servers/", {
+      const result = await axios("http://217.69.10.141:5000/network-package", {
         params: {
-          ID: id
+          node_id: "172.17.0.7",
+          from: "2020-10-04 11:50:23",
+          to: "2020-12-04 11:50:23"
         }
       });
       setData(result.data);
     };
     fetchData();
   }, []);
-  
-  const serverTable = {
-    timestamp: data.map((status) => status.timestamp),
-    protocol: [data.map((status) => status.protocol)],
-    Size: [data.map((status) => status.Size)],
-    destinationIp: [data.map((status) => status.destinationIp)],
-    destinationPort: [data.map((status) => status.destinationPort)],
-    sourceIp: [data.map((status) => status.sourceIp)],
-    sourceName: [data.map((status) => status.sourceName)],
-    sourcePort: [data.map((status) => status.sourcePort)]
-  };
+
+  const serverTable = [{
+    
+    Timestamp: data.map((status) => status.timestamp),
+    Protocol: data.map((status) => status.protocol),
+    Size: data.map((status) => status.size),
+    Destination: data.map((status) => status.dst),
+    DestinationPort: data.map((status) => status.dst_port),
+    Source: data.map((status) => status.src),
+    SourcePort: data.map((status) => status.src_port),
+    Layer: data.map((status) => status.layer)
+  }];
 
 
   const [openNotification, setOpenNotification] = React.useState(null);
@@ -183,7 +192,7 @@ export default function ServerDetails() {
             <CardHeader color="success">
               <ChartistGraph
                 className="ct-chart"
-                data={dailySalesChart.data}
+                data={chartCPU}
                 type="Line"
                 options={dailySalesChart.options}
                 listener={dailySalesChart.animation}
@@ -192,7 +201,7 @@ export default function ServerDetails() {
             <CardBody>
               <h4 className={classes.cardTitle}> CPU </h4>{" "}
               <p className={classes.cardCategory}>
-                <span className={classes.successText}>39 %</span> Current CPU
+                <span className={classes.successText}>  </span> Current CPU
                 load{" "}
               </p>{" "}
               <p>
@@ -211,7 +220,7 @@ export default function ServerDetails() {
             <CardHeader color="warning">
               <ChartistGraph
                 className="ct-chart"
-                data={dailySalesChart.data}
+                data={chartRAM}
                 type="Line"
                 options={dailySalesChart.options}
                 listener={dailySalesChart.animation}
@@ -220,7 +229,7 @@ export default function ServerDetails() {
             <CardBody>
               <h4 className={classes.cardTitle}> RAM </h4>{" "}
               <p className={classes.cardCategory}>
-                <span className={classes.successText}>39 %</span> Current RAM
+                <span className={classes.successText}>  </span> Current RAM
                 usage{" "}
               </p>{" "}
               <p>
@@ -239,10 +248,10 @@ export default function ServerDetails() {
             <CardHeader color="danger">
               <ChartistGraph
                 className="ct-chart"
-                data={completedTasksChart.data}
+                data={chartBandwidth}
                 type="Line"
-                options={completedTasksChart.options}
-                listener={completedTasksChart.animation}
+                options={dailySalesChart.options}
+                listener={dailySalesChart.animation}
               />{" "}
             </CardHeader>{" "}
             <CardBody>
@@ -250,7 +259,7 @@ export default function ServerDetails() {
               <p className={classes.cardCategory}>
                 {" "}
                 Current Bandwith{" "}
-                <span className={classes.successText}> 39 %</span>{" "}
+                <span className={classes.successText}>  </span>{" "}
               </p>{" "}
               <p>
                 <Multiselect
@@ -270,22 +279,22 @@ export default function ServerDetails() {
           <h4 className={classes.cardTitleWhite}> Network Packages </h4>{" "}
         </CardHeader>{" "}
         <CardBody>
-        <ToolkitProvider
-          keyField="Protocol"
-          data={products}
-          columns={columns}
-          search
+          <ToolkitProvider
+            keyField="Protocol"
+            data={serverTable}
+            columns={columns}
+            search
           >
             {
               props => (
                 <div>
-                  <SearchBar { ...props.searchProps } />
-                  <BootstrapTable { ...props.baseProps}
-            keyField="Protocol"
-            data={products}
-            columns={columns}
-            striped
-          />
+                  <SearchBar {...props.searchProps} />
+                  <BootstrapTable {...props.baseProps}
+                    keyField="Protocol"
+                    data={serverTable}
+                    columns={columns}
+                    striped
+                  />
 
                 </div>
               )
